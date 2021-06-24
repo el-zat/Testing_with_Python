49f756d16d9b57cac82cdfefaca6a3f816c5d5c1
el-zat <elena.zatykina@gmail.com>
2021-06-24 18:29:40 +0200

    conftest created

---
+++ conftest.py
@@ -0,0 +1,9 @@
+from fixture.application_group import Application
+import pytest
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
