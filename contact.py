11687b1ae94ffe782d66a73a99a8784804796e97
el-zat <elena.zatykina@gmail.com>
2021-06-24 14:39:42 +0200

    contact.py created and Parameter Object added

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, firstname, lastname, nickname, company, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from contact import Contact
+
 
 class test_add_contact(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
 
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, "admin", "secret")
         self.open_add_new_contact(wd)
-        self.create_new_contact(wd)
+        self.create_new_contact(wd, Contact("Ivan", "Petrov", "IvPet", "SAP", "ivan.petrov@mail.com"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -29,23 +26,23 @@ class test_add_contact(unittest.TestCase):
         # return to home page
         wd.find_element_by_link_text("home").click()
 
-    def create_new_contact(self, wd):
+    def create_new_contact(self, wd, contact):
         # create contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Ivan")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Petrov")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("IvPet")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("SAP")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("ivan.petrov@mail.com")
+        wd.find_element_by_name("email").send_keys(contact.email)
         # submit new contact
         wd.find_element_by_name("submit").click()
 
@@ -53,13 +50,14 @@ class test_add_contact(unittest.TestCase):
         # open add new contact
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username="admin", password="secret"):
         # login
+        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
     def open_home_page(self, wd):
