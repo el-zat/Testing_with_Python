f76bfc0c9dae2b24d2edc4284f651d8676609271
el-zat <elena.zatykina@gmail.com>
2021-06-25 15:52:29 +0200

    tests after application and login logout modified

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,8 @@
 from model.contact import Contact
 
 
-def test_add_contact(app):
-    app.session.login("admin", "secret")
-    app.contact_helper.create_new_contact(Contact("Ivan", "Petrov", "IvPet", "SAP", "ivan.petrov@mail.com"))
-    app.session.logout()
+def test_add_contact(app_con):
+    app_con.contact_helper.create_new_contact(Contact("Ivan", "Petrov", "IvPet", "SAP", "ivan.petrov@mail.com"))
+
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login("admin", "secret")
     app.group_helper.create_group(Group("group2", "group2", "group2"))
-    app.session.logout()
+#
+#
+# def test_add_empty_group(app):
+#     app.group_helper.create_group(Group("", "", ""))
 
-
-def test_add_empty_group(app):
-    app.session.login("admin", "secret")
-    app.group_helper.create_group(Group("", "", ""))
-    app.session.logout()
 
