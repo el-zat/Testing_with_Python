25b221ee8ca1c3f0b0bfd646c298fa344ec4da13
el-zat <elena.zatykina@gmail.com>
2021-06-25 13:11:09 +0200

    test edit_first_contact modified

--- test/test_edit_first_contact.py
+++ test/test_edit_first_contact.py
@@ -1,6 +1,8 @@
+from model.contact import Contact
 
 
 def test_edit_first_contact(app_con):
     app_con.session.login("admin", "secret")
-    app_con.contact_helper.edit_first_contact()
+    app_con.contact_helper.modify_first_contact(Contact(firstname="", lastname="", nickname="", company="", email=""))
+    app_con.contact_helper.modify_first_contact(Contact(firstname="Joe", lastname="Biden", nickname="JoeBid", company="White House", email="j.biden@gmail.com"))
     app_con.session.logout()
