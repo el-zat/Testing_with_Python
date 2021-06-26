a71fc9774d6c22b150018a75e65686e26037dfef
el-zat <77495664+el-zat@users.noreply.github.com>
2021-06-26 20:08:40 +0200

    Delete test_edit_first_contact.py

--- test/test_edit_first_contact.py
+++
@@ -1,7 +0,0 @@
-from model.contact import Contact
-
-
-def test_edit_first_contact(app):
-    app.contact_helper.modify_first_contact(Contact(firstname="", lastname="", nickname="", company="", email=""))
-    app.contact_helper.modify_first_contact(Contact(firstname="Joe", lastname="Biden", nickname="JoeBid", company="White House", email="j.biden@gmail.com"))
-
