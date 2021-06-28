922c82ccf82f82d5f2edf15c2eba8cd65b6c1e52
el-zat <elena.zatykina@gmail.com>
2021-06-28 20:53:22 +0200

    group and contact random modification and deletion

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,13 +1,15 @@
 from model.contact import Contact
+from random import randrange
 
 
-def test_delete_first_contact(app):
+def test_delete_some_contact(app):
     if app.contact_helper.count() == 0:
         app.contact_helper.create_new_contact(Contact("Ippolit", "Vorobyaninov", "Kisa", "ZAGS", "ipp.vorob@mail.com"))
     old_contacts = app.contact_helper.get_contact_list()
-    app.contact_helper.delete_first_contact()
+    index = randrange(len(old_contacts))
+    app.contact_helper.delete_contact_by_index(index)
     assert len(old_contacts)-1 == app.contact_helper.count()
     new_contacts = app.contact_helper.get_contact_list()
-    old_contacts[0:1] = []
+    old_contacts[index:index+1] = []
     assert old_contacts == new_contacts
 
