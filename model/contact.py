03bef87028b4f1e04a9520ae6ad7247b4e37a906
el-zat <elena.zatykina@gmail.com>
2021-06-29 23:13:34 +0200

    check phones, firstname,lastname,email and address of a random contact

--- model/contact.py
+++ model/contact.py
@@ -3,22 +3,29 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, nickname=None, company=None, email=None, id=None):
+    def __init__(self, firstname=None, lastname=None, nickname=None, company=None, email=None, id=None,
+                 all_phones_from_home_page=None, address=None, homephone=None, mobilephone=None, workphone=None):
         self.firstname = firstname
         self.lastname = lastname
         self.nickname = nickname
         self.company = company
         self.email = email
         self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.address = address
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
 
     def __repr__(self):
         return "%s:%s" % (self.id, self.firstname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname \
-               # and \
-               # self.lastname == other.lastname and self.nickname == other.nickname and self.company == other.company \
-               # and self.email == other.email
+
 
     def id_or_max(self):
         if self.id:
