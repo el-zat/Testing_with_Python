2999a3d6b514be856a285dda4f7e3a46454274ba
el-zat <elena.zatykina@gmail.com>
2021-06-30 16:24:46 +0200

    load data from json

--- conftest.py
+++ conftest.py
@@ -1,19 +1,27 @@
+import json
+
 from fixture.application import Application
 import pytest
+import json
+import os.path
+
 
 fixture = None
+target = None
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        fixture = Application()
-
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login("admin", "secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -26,3 +34,6 @@ def stop(request):
     return fixture
 
 
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
