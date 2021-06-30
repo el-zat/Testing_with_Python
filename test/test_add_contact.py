ceba24854d7d434387dae5bbbefe960906a5c858
el-zat <elena.zatykina@gmail.com>
2021-06-30 12:32:32 +0200

    check phones, firstname,lastname,email and address of a random contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,10 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact_helper.get_contact_list()
-    contact = Contact("Ivan", "Petrov", "IvPet", "SAP", "ivan.petrov@mail.com")
+    contact = Contact(firstname="Ivan", lastname="Petrov", nickname="IvPet", company="SAP",
+                      email="ivan.petrov@mail.com", homephone="45448", mobilephone="2416787",
+                      workphone="478466", address="Longstreet,20 \n 58963 New York")
+
     app.contact_helper.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact_helper.count()
     new_contacts = app.contact_helper.get_contact_list()
