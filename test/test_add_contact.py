df558dcd7eb4addc655e7131822bfd9a00abff23
el-zat <elena.zatykina@gmail.com>
2021-07-17 12:44:38 +0200

    Task28-1

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,13 +3,12 @@
 from model.contact import Contact
 
 
-def test_add_contact(app, json_contacts, check_ui):
+def test_add_contact(app, json_contacts, db, check_ui):
     contact = json_contacts
     old_contacts = app.contact_helper.get_contact_list()
     app.contact_helper.create_new_contact(contact)
     new_contacts = app.contact_helper.get_contact_list()
+    assert len(old_contacts) + 1 == app.contact_helper.count()
     old_contacts.append(contact)
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact_helper.get_contact_list(),
-                                                                     key=Contact.id_or_max)
-
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
---
+++ test/еуые.py
@@ -0,0 +1,28 @@
+from model.contact import Contact
+from model.group import Group
+from fixture.orm import ORMFixture
+import random
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+
+def test_add_contact_into_group(app):
+    old_contacts = db.get_contact_list()
+    if len(old_contacts) == 0:
+        app.contact_helper.create_new_contact(Contact(firstname="firstname_1"))
+    old_groups = db.get_group_list()
+    if len(old_groups) == 0:
+        app.group_helper.create_group(Group(name="name_1", header="header_1", footer="footer_1"))
+
+    group = random.choice(db.get_group_list())
+
+    old_contacts_in_group = db.get_contacts_in_group(Group(id=group.id))
+    old_contacts_not_in_groups = db.get_contacts_not_in_group(Group(id=group.id))
+    contact = random.choice(old_contacts_not_in_groups)
+    app.contact_helper.add_contact_into_group(id=contact.id, group_id=group.id)
+    new_contacts_not_in_groups = db.get_contacts_not_in_group(Group(id=group.id))
+    new_contacts_in_group = db.get_contacts_in_group(Group(id=group.id))
+    assert len(old_contacts_not_in_groups) == len(new_contacts_in_group)
+    assert len(old_contacts_in_group) == len(new_contacts_not_in_groups)
+    old_contacts_in_group.append(contact)
+    assert sorted(old_contacts_in_group, key=Contact.id_or_max) == sorted(new_contacts_in_group, key=Contact.id_or_max)
