a5509d15bdb885369b3366cdd9b5a285c224a8ff
el-zat <elena.zatykina@gmail.com>
2021-07-20 12:16:36 +0200

    robot framework for add,delete and modify contact

--- rf/AddressBook.py
+++ rf/AddressBook.py
@@ -4,6 +4,7 @@ import json
 import os.path
 from fixture.db import DbFixture
 from model.group import Group
+from model.contact import Contact
 
 
 class AddressBook:
@@ -28,5 +29,36 @@ class AddressBook:
         self.dbfixture.destroy()
         self.fixture.destroy()
 
-    def create_group(self, name, header, footer):
-        self.fixture.group_helper.create_group(Group(name=name, header=header, footer=footer))
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def create_group(self, group):
+        self.fixture.group_helper.create_group(group)
+
+    def group_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
+    def delete_group(self, group):
+        self.fixture.group_helper.delete_group_by_id(group.id)
+
+
+    def new_contact(self, firstname, lastname, email):
+        return Contact(firstname=firstname, lastname=lastname, email=email)
+
+    def get_contact_list(self):
+        return self.dbfixture.get_contact_list()
+
+    def create_new_contact(self, contact):
+        self.fixture.contact_helper.create_new_contact(contact)
+
+    def contact_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Contact.id_or_max) == sorted(list1, key=Contact.id_or_max)
+
+    def delete_contact(self, contact):
+        self.fixture.contact_helper.delete_contact_by_id(contact.id)
+
+    def modify_contact(self, contact, new_contact_data):
+        self.fixture.contact_helper.modify_contact_by_id(contact.id, new_contact_data)
