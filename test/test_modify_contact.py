4cae2bb63e1b2e28d726a67bfda5810ae9d643c5
el-zat <elena.zatykina@gmail.com>
2021-07-30 16:14:14 +0200

    tests modify group and contact for task 20

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,12 +7,14 @@ def test_modify_some_contact(app, db, check_ui):
         app.contact_helper.create_new_contact(Contact("Ippolit", "Vorobyaninov", "Kisa", "ZAGS", "ipp.vorob@mail.com"))
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
-    new_contact = Contact(firstname="Joe", lastname="Biden", nickname="JoeBid", company="White House",
-                          email="joe.biden@gmail.com")
-    app.contact_helper.modify_contact_by_id(contact.id, new_contact)
+    index = old_contacts.index(contact)
+    modified_contact = Contact(firstname="Joe", lastname="Biden", nickname="JoeBid", company="White House",
+                               email="joe.biden@gmail.com")
+    app.contact_helper.modify_contact_by_id(contact.id, modified_contact)
     new_contacts = db.get_contact_list()
+    old_contacts[index] = modified_contact
     assert len(old_contacts) == len(new_contacts)
-    assert sorted(new_contacts, key=Contact.id_or_max) == sorted(old_contacts, key=Contact.id_or_max)
+    assert old_contacts == new_contacts
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact_helper.get_contact_list(),
                                                                      key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -7,9 +7,12 @@ def test_modify_group_name(app, db, check_ui):
         app.group_helper.create_group(Group(name="test1"))
     old_groups = db.get_group_list()
     group = random.choice(old_groups)
-    new_group = Group(name="Group_new")
-    app.group_helper.modify_group_by_id(group.id, new_group)
+    index = old_groups.index(group)
+    modified_group = Group(name="Group_new_789")
+    app.group_helper.modify_group_by_id(group.id, modified_group)
     new_groups = db.get_group_list()
+    old_groups[index] = modified_group
     assert len(old_groups) == len(new_groups)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group_helper.get_group_list(), key=Group.id_or_max)
+    assert old_groups == new_groups
